9a9818636ef001501977dd12349c30d460e86b9a
prashanth-kj <kjprasanth98@gmail.com>
2023-10-13 20:26:56 +0530

    Express-crud

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //  import express from 'express';        // this is ES script module add type:module in package.json
 
-const express =require('express') // this is common js module
+const express =require('express') // this is common js module used
 const AppRouters=require('./src/routes')
  const app= express();
     app.use(express.json())
