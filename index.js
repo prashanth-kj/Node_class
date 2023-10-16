6396787f377770019ee46943306876e72e4ec842
prashanth-kj <kjprasanth98@gmail.com>
2023-10-16 13:29:23 +0530

    cors updated

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 const express =require('express') // this is common js module used
 
 const dotenv=require('dotenv')
+const cors=require('cors')
+const AppRouters=require('./src/routes')
  dotenv.config()
 
   const PORT = process.env.PORT
-const AppRouters=require('./src/routes')
+
  const app= express();
+    app.use(cors())
     app.use(express.json())
     app.use('/',AppRouters)
        
